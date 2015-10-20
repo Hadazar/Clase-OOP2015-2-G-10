078136c6b2ba08bf97e1de36a7da5a133d7ff54d
jaossacas <jaossacas@unal.edu.co>
2015-10-20 17:56:41 -0500

    Update Cheque.java

--- Parcial 1/UNBank/src/Paquete/Cheque.java
+++ Parcial 1/UNBank/src/Paquete/Cheque.java
@@ -3,7 +3,7 @@ package Paquete;
 public class Cheque extends ProductoBancario{
 	
 	String beneficiario;
-	double monto;
+	double mont;
 	String pagador;
 	
 	void setAtributos(String beneficiario, double monto, String pagador){
