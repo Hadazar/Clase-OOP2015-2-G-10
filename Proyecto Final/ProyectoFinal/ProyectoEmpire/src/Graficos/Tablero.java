cd56a6a2f9d9e8d623146b7a199b4b747fc981a0
Hadazar <hadazar@unal.edu.co>
2015-12-03 15:06:09 -0500

    Update Tablero.java

--- Proyecto Final/ProyectoFinal/ProyectoEmpire/src/Graficos/Tablero.java
+++ Proyecto Final/ProyectoFinal/ProyectoEmpire/src/Graficos/Tablero.java
@@ -5,11 +5,18 @@ import javax.swing.*;
 
 public class Tablero extends JFrame{
     
+    private JButton NuevaPartida;
+    
     public Tablero(){
         super( "Imperio de mesa" );
         setSize( 1500, 750 );
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
+        setDefaultCloseOperation( EXIT_ON_CLOSE );
+        setVisible( true );
+        setLayout (new FlowLayout());
+        
+        NuevaPartida = new JButton( "Nueva Partida" );
+        add(Nuevapartida);
+        
     }
     
     
